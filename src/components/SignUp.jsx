1f777e7270f81d637377093f8b6bde255eecf1b1
Palak Choudhary <palakc456@gmail.com>
2025-02-17 18:20:34 +0530

    commit 3

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import '../Components/SignUP.css'
+import './SignUp.css'
 
-const SignUp = () => {
+function SignUp() {
     return (
-        <div className='signup_section'>
-            <div className='signup_img'>
-                <img src="https://assets-news.housing.com/news/wp-content/upload"/>
+        <div className="signup_section">
+            <div className="signup_img">
+                <img src="https://media.istockphoto.com/id/1165384568/photo/europe-modern-complex-of-residential-buildings.jpg?s=612x612&w=0&k=20&c=iW4NBiMPKEuvaA7h8wIsPHikhS64eR-5EVPfjQ9GPOA=" alt="Building" />
             </div>
-            <div className='signup_form'>
-                <h3>Sign Up</h3>
-                <label htmlFor="email">Email : </label>
-                <input type="email" />
-                <label htmlFor="password">Password : </label>
-                <input type="password" />
-                <label htmlFor="role">Role : </label>
-                <select name="role" id="role">
-                    <option value="">Select</option>
-                    <option value="Admin">Admin</option>
-                    <option value="Resident">Resident</option>
-                    <option value="others">others</option>
-                </select>
-                <button>Sign Up</button>
+            <div className="signup_form">
+                <h2>Sign Up</h2>
+                <div className="form_group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" placeholder="Enter your email" />
+                </div>
+                <div className="form_group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" placeholder="Enter your password" />
+                </div>
+                <div className="form_group">
+                    <label htmlFor="role">Role</label>
+                    <select id="role">
+                        <option value="">Select your role</option>
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                        <option value="others">Others</option>
+                    </select>
+                </div>
+                <button type="submit">Sign Up</button>
             </div>
         </div>
     )
